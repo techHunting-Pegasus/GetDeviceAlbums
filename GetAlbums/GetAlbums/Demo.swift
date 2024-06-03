1b3421b01f85dec71024d20cf4c8085dd7c8fd29
Ishpreet singh <ishpreetsingh8386@gmail.com>
2024-06-03 16:07:36 +0530

    Add some file in project

---
+++ GetAlbums/GetAlbums/Demo.swift
@@ -0,0 +1,13 @@
+//
+//  Demo.swift
+//  GetAlbums
+//
+//  Created by John on 03/06/24.
+//
+
+import Foundation
+public class Demo {
+    public func log(message:Any){
+        print(message)
+    }
+}
---
+++ GetAlbums/GetAlbums/GetAlbums.h
@@ -0,0 +1,18 @@
+//
+//  GetAlbums.h
+//  GetAlbums
+//
+//  Created by John on 03/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GetAlbums.
+FOUNDATION_EXPORT double GetAlbumsVersionNumber;
+
+//! Project version string for GetAlbums.
+FOUNDATION_EXPORT const unsigned char GetAlbumsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GetAlbums/PublicHeader.h>
+
+
